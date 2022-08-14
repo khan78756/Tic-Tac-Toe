12e2837dac865d7a51548ac6bef5bfee57cdbb76
abdullahkhan <abdullahkhan78756@gmail.com>
2022-08-14 16:43:59 +0500

    Tic Tak Toe

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -12,20 +12,13 @@ import android.widget.TextView
 import androidx.annotation.IntegerRes
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
+import com.example.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener
 {
-    lateinit var b1:Button
-    lateinit var b2:Button
-    lateinit var b3:Button
-    lateinit var b4:Button
-    lateinit var b5:Button
-    lateinit var b6:Button
-    lateinit var b7:Button
-    lateinit var b8:Button
-    lateinit var b9:Button
-    lateinit var screen:TextView
-    lateinit var turnX:TextView
+
+
+    private lateinit var binding: ActivityMainBinding
 
     //For Locking of the Buttons
 
@@ -49,7 +42,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val exit=findViewById<Button>(R.id.btnexit)
 
@@ -58,33 +53,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener
             finish()
         }
 
-        //for get Id
-        b1=findViewById(R.id.b1)
-        b2=findViewById(R.id.b2)
-        b3=findViewById(R.id.b3)
-        b4=findViewById(R.id.b4)
-        b5=findViewById(R.id.b5)
-        b6=findViewById(R.id.b6)
-        b7=findViewById(R.id.b7)
-        b8=findViewById(R.id.b8)
-        b9=findViewById(R.id.b9)
-        screen=findViewById(R.id.screen)
-        turnX=findViewById(R.id.turnX)
-
-
         //Clicklisner
 
         fillpos= intArrayOf(-1,-1,-1,-1,-1,-1,-1,-1,-1)
 
-        b1.setOnClickListener(this)
-        b2.setOnClickListener(this)
-        b3.setOnClickListener(this)
-        b4.setOnClickListener(this)
-        b5.setOnClickListener(this)
-        b6.setOnClickListener(this)
-        b7.setOnClickListener(this)
-        b8.setOnClickListener(this)
-        b9.setOnClickListener(this)
+        binding.b1.setOnClickListener(this)
+        binding.b2.setOnClickListener(this)
+        binding.b3.setOnClickListener(this)
+        binding.b4.setOnClickListener(this)
+        binding.b5.setOnClickListener(this)
+        binding.b6.setOnClickListener(this)
+        binding. b7.setOnClickListener(this)
+        binding.b8.setOnClickListener(this)
+        binding. b9.setOnClickListener(this)
     }
 
     //Funtion For functionalities
@@ -109,14 +90,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener
             buttonclicked.setText("0")
             activeplayer = player2
 
-            screen.setText("Player-2 Turn")
+           binding.screen.setText("Player-2 Turn")
             buttonclicked.setTextColor(Color.WHITE)
 
         } else
         {
             buttonclicked.setText("x")
             activeplayer = player1
-            screen.setText("Player-1 Turn")
+            binding.screen.setText("Player-1 Turn")
             buttonclicked.setTextColor(Color.WHITE)
 
         }
@@ -197,16 +178,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener
       fillpos=intArrayOf(-1,-1,-1,-1,-1,-1,-1,-1,-1)
         activeplayer=player1
         gameactive=true
-           screen.setText("Player-1 Turn")
-           b1.setText("")
-           b2.setText("")
-           b3.setText("")
-           b4.setText("")
-           b5.setText("")
-           b6.setText("")
-           b7.setText("")
-           b8.setText("")
-           b9.setText("")
+        binding.screen.setText("Player-1 Turn")
+           binding.b1.setText("")
+        binding.b2.setText("")
+        binding.b3.setText("")
+        binding.b4.setText("")
+        binding. b5.setText("")
+        binding. b6.setText("")
+        binding. b7.setText("")
+        binding.b8.setText("")
+        binding. b9.setText("")
     }
 
 }
